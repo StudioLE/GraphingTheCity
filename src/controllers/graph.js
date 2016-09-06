ba6a8e685c535599de9362c07bb816c07138ddb2
Laurence Elsdon <git@elsdon.me>
2016-09-06 16:50:18 +0100

    Visualise graphs with d3 rather than cytoscape

--- src/controllers/graph.js
+++ src/controllers/graph.js
@@ -37,10 +37,8 @@ angular.module('app.graph', ['ngRoute'])
 
   var nodes = _.map(places, function(place) {
     return {
-      data: {
-        id: place['@id'],
-        label: place.name
-      },
+      id: place['@id'],
+      name: place.name
       // classes: 'bg-blue',
       // selected: true,
       // selectable: true,
@@ -49,91 +47,90 @@ angular.module('app.graph', ['ngRoute'])
     }
   })
 
-  var cy = window.cy = cytoscape({
-    container: document.getElementById('cy'),
-    // userZoomingEnabled: false,
-    // userPanningEnabled: false,
-    textureOnViewport: true,
-    boxSelectionEnabled: false,
-    layout: {
-      name: criteria.layout || 'circle' // 'circle' // 'cose-bilkent' 'dagre' 'grid' 'spread'
-    },
-    elements: {
-      nodes: nodes,
-      edges: connections
-    },
-    style: [
-      {
-        selector: 'node',
-        style: {
-          // 'height': 40,
-          // 'width': 40,
-          'label': 'data(label)',
-          'background-color': '#FF9800',
-          'color': '#ffffff',
-          'font-size': '10px',
-          'text-valign': 'bottom',
-          'text-halign': 'right',
-        }
-      },
-
-      {
-        selector: 'edge',
-        style: {
-          'width': 3,
-          'line-color': '#FF9800'
-        }
-      }
-    ],
+  // Cytoscape had a 'data' sub object, d3 does not so lets convert
+  var links = _.map(connections, function(link) {
+    // return link.data
+    return {
+      source: link.data.source,
+      target: link.data.target
+    }
   })
 
-  // cy.style('node {height:40;width:80;shape:rectangle;background-color:#ccc;label:data(label);text-valign:center;text-halign:center;color:#fff;}edge{width:3;}.bg-navy{background-color:#001F3F;}.bg-blue{background-color:#0074D9;}.bg-aqua{background-color:#7FDBFF;}.bg-teal{background-color:#39CCCC;}.bg-olive{background-color:#3D9970;}.bg-green{background-color:#2ECC40;}.bg-lime{background-color:#01FF70;}.bg-yellow{background-color:#FFDC00;}.bg-orange{background-color:#FF851B;}.bg-red{background-color:#FF4136;}.bg-fuchsia{background-color:#F012BE;}.bg-purple{background-color:#B10DC9;}.bg-maroon{background-color:#85144B;}.bg-white{background-color:#fff;}.bg-gray{background-color:#aaa;}.bg-silver{background-color:#ddd;}.bg-black{background-color:#111;}')
-  // $scope.layout = 'cose'
-  $scope.changeLayout = function() {
-    Criteria.set($scope.criteria())
-    cy.layout({
-      name: $scope.criteria().layout
-    })
+  console.log(nodes)
+
+  console.log(links)
+
+  // d3 graph example from: https://bl.ocks.org/mbostock/4062045
+  // d3 responsive viewport: http://stackoverflow.com/questions/9400615/whats-the-best-way-to-make-a-d3-js-visualisation-layout-responsive
+
+  var svg = d3.select("svg"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height")
+
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+  var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+  var link = svg.append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(links)
+    .enter().append("line")
+    .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  var node = svg.append("g")
+      .attr("class", "nodes")
+    .selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+      .attr("r", 5)
+      .attr("fill", function(d) { return color(d.group); })
+      .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
+
+  node.append("title")
+      .text(function(d) { return d.id; });
+
+  simulation
+      .nodes(nodes)
+      .on("tick", ticked);
+
+  simulation.force("link")
+      .links(links);
+
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
+  // });
+
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
   }
 
-  $scope.export = function() {
-    var content = JSON.stringify(cy.json())
-
-    window.webkitRequestFileSystem(window.TEMPORARY, 1024*1024, saveJSON);
-
-    function saveJSON(localstorage) {
-      localstorage.root.getFile('citygraph.json', {create: true}, function(DatFile) {
-        DatFile.createWriter(function(DatContent) {
-          var blob = new Blob([content], {type: "text/json"})
-          DatContent.write(blob)
-        })
-      })
-    }
-
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
   }
 
-  $scope.savePNG = function() {
-    var content = cy.png({
-      full: true
-    })
-
-    // $scope.pngSrc = content
-
-    // $location.path(content)
-
-    window.webkitRequestFileSystem(window.TEMPORARY, 1024*1024, savePNGFile);
-
-    function savePNGFile(localstorage) {
-      localstorage.root.getFile('citygraph.png', {create: true}, function(DatFile) {
-        DatFile.createWriter(function(DatContent) {
-          var blob = new Blob([content], {type: "image/png"})
-          DatContent.write(blob)
-        })
-      })
-    }
-
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
   }
-
-  $scope.pngSrc = ''
 
 })
