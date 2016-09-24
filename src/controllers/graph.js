c78fbdc928b98dfd380918fa351726c1d2b04156
Laurence Elsdon <git@elsdon.me>
2016-09-24 16:02:20 +0100

    Fixed graph charge value

--- src/controllers/graph.js
+++ src/controllers/graph.js
@@ -84,7 +84,7 @@ angular.module('app.graph', ['ngRoute'])
 
   var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody().strength(-120))
+    .force("charge", d3.forceManyBody().strength(-2))
     // .gravity(.5)
     .force("center", d3.forceCenter(width / 2, height / 2));
 
